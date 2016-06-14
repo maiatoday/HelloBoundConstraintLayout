11920efe2c66ff115533318ded024c7caf998522
maiatoday <maiatoday@gmail.com>
2016-06-14 20:56:43 +0200

    Initial commit

---
+++ app/src/main/java/net/maiatoday/helloboundconstraintlayout/InfoItem.java
@@ -0,0 +1,13 @@
+package net.maiatoday.helloboundconstraintlayout;
+
+/**
+ * Created by maia on 2016/06/13.
+ */
+
+public class InfoItem {
+    String imageUrl;
+    String title;
+    String description;
+    String value;
+    String longThinText;
+}
